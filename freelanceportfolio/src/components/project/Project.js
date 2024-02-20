a816fba063cc5197083f39b30daba57661383333
TohaDoku <143796346+TohaDoku@users.noreply.github.com>
2024-02-20 16:54:00 +0300

    Начал разработку страницы проекта

--- freelanceportfolio/src/components/project/Project.js
+++ freelanceportfolio/src/components/project/Project.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
-
 import './Project.css'
-
 
 
 export class Project extends Component {
   render() {
-
     return (
-        <>
+        <> 
             <li className="project">
                 <a href="./project_page.html">
                     <img src={this.props.img} alt={this.props.title} className="project__img" />
--- freelanceportfolio/src/pages/Project.js
+++ freelanceportfolio/src/pages/Project.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
 import BtnGitHub from '../components/buttonGitHub/BtnGitHub'
 
+import {projects} from '../components/helpers/projectList'
+import {useParams} from 'react-router-dom'
 
 export class Project extends Component {
   render() {
+    
+    const project = projects[this.props.id]
+
     return (
       <div>
         <main className="section">
             <div className="container">
                 <div className="project-details">    
-                    <h1 className="title-1">Video service</h1>
+                    <h1 className="title-1">{project.id}</h1>
                     <img src="./img/projects/02-big.jpg" alt="" className="project-details__cover" />
                     <div className="project-details__desc">
                         <p>Skills: React, Node.js, MongoDB</p>
